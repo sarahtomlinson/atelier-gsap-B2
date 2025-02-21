1993d97d2851c68946ccb4ec236ddfe5d9c1ee2c
sarah3 <sarah@MacBook-Pro-de-MARTINS.local>
2025-02-21 11:34:04 +0100

    Last update on GSAP

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-//gsap modules
+//GSAP modules
 gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
 
-//gsap  animation------------------------------------
+//GSAP  animation 1------------------------------------
 gsap.to("#introTitle",{
     rotation : 360,
-    duration : 2,
+    duration : 3,
 });
 
-//gsap media match
+//GSAP media match
 /*
 let mm = gsap.matchMedia();
 mm.add({
@@ -18,31 +18,12 @@ mm.add({
 
 Draggable.create("#introTitle")
 
-//gsap animation window rotation
+//GSAP animation window 2 apparition-----------------------------------------------
 
-gsap.to("#window1", {
-    scrollTrigger:{
-        target: "#section1", 
-        markers: true, 
-        id: "section1",
-        start:"top 0%",
-        end: "top 100%",
-        toggleActions: "play none reverse none",
-        scrub: 1
-
-
-    },
-    rotation: 360,
-    duration: 2, 
-})
-
-
-//gsap animation window 2 apparition
-
-gsap.from("window2", {
+gsap.from("onedirection", {
     scrollTrigger:{
         trigger:"#section2",
-        markers: true,
+        //markers: true,
         start: "top 80%",
         end: "bottom 50%", 
         toggleActions: "play reverse none reset",
@@ -52,44 +33,78 @@ gsap.from("window2", {
 duration: 1,
 
 
-})
+}) 
 
 
-//gsap animation - window 3 motion path
-
-gsap.to("#window3", {
-    duration: 5,
-    motionPath: {
-        path: "M203.529 11C189.862 16.3333 164.529 34.4 172.529 64C180.529 93.6 211.862 97.6667 226.529 96C259 96 294.329 133.4 245.529 179C196.729 224.6 207.862 287.333 219.529 313C233.196 334 245.329 381 184.529 401",
-    },
-    onComplete:()=> {
-        document.getElementById('titleSec3').style.color="red"
-    }
-})
-
-//GSAp animation - window 4 timeline
-let timelineWindow = gsap.timeline();
-timelineWindow.to('#window4', {x: 100,})
-    .to('#window4', {y: 100,})
-    .to('#window4', { x: -100,})
-    .to('#window4', {y: -100,});
+//GSAP animation - window 4 to 5  ---------------------------------------------
+let timeline4To5 = gsap.timeline({ //on créer une timeline et scroll pour introduire un succession de point afin de former une sorte de motion path à l'aide des données de x et y.
+    scrollTrigger:{
+        trigger: "#sec4To5", 
+       //markers: true, 
+        id: "section4",
+        
+        start:"top 60%",
+        end: "bottom 40%",
+        toggleActions: "play none reverse none",
+        scrub: 1
     
+    },
+});
 
-//draggable 
-Draggable.create(".name",{
+timeline4To5.to('.micro', {x: '25vw',y:'-50px', }) // on determine les positions du micro
+    .to('.micro', {x: '50vw',y:'50px', })
+    .to('.micro', {x: '75vw',y:'-50px', })
+    .to('.micro', {x: '100vw',y:'50px', });
+
+//Draggable  section 3-------------------------------------------------
+Draggable.create(".name",{  //on rend draggable les oms des chanteurs pour les mettre dans les deposit zone
     type:"x,y",
     bounds: "#section3",
 
     onDrag: function(){
         //document.querySelector('.harry').style.width = "250px";
     },
-   
-
-    onDragend: function(){
-        //document.querySelector('.harry').style.width = "200px";
-    },
-
+    
 })
+
+// Draggable section 5 ---------------------------------------------
+// Mauvais
+Draggable.create(".cover", {       
+    type: "x,y",
+    bounds: "#section5",
+    onDragEnd: function() {
+
+        console.log(document.getElementById("depositSec5")); 
+        let depositZone5 = document.getElementById("depositSec5");
+
+        if (this.hitTest(depositZone5, "80%")) {  // Augmenter la tolérance à 80%
+            depositZone5.style.backgroundColor = "#eb3e3b"; //on attribue à la deposite zone la couleur rouge si l'element placé est le mauvais
+            console.log("Bad");
+        } else {
+            console.log("Retour à la position initiale");
+            gsap.to(this.target, { duration: 0.5, x: 0, y: 0, ease: "power2.out" }); //on ajoute une petite animation pour fluidifer la position
+        }
+    },
+});
+
+// Draggable section 5 ---------------------------------------------
+// GOOD
+Draggable.create(".goodCover", {
+    type: "x,y",
+    bounds: "#section5",
+    onDragEnd: function() {
+        console.log(document.getElementById("depositSec5"));  
+        let depositZone5 = document.getElementById("depositSec5");
+
+        if (this.hitTest(depositZone5, "80%")) {  // Augmenter la tolérance à 80%
+            depositZone5.style.backgroundColor = "#ccf5bd";
+            console.log("Good"); //on attribue à la deposite zone la couleur verte si l'element placé est le bon
+        } else {
+            console.log("Retour à la position initiale");
+            gsap.to(this.target, { duration: 0.5, x: 0, y: 0, ease: "power2.out" });  //on ajoute une petite animation pour fluidifer la position
+        }
+    },
+});
 
    
 
